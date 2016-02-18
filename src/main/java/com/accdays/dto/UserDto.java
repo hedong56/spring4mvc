b744f85baffcd60d86ebcd3bec2151a39cee8a66
hedong56 <867459110@qq.com>
2016-02-18 19:55:01 +0800

    添加一个UserDto实体

---
+++ src/main/java/com/accdays/dto/UserDto.java
@@ -0,0 +1,27 @@
+package com.accdays.dto;
+
+public class UserDto {
+	
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	private String sex;
+	private String address;
+
+}
